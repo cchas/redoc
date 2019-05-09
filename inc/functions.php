41464a44cce9708c2eb45b97bf7ea78dd87cd107
=César Chas <=cesar@respirainternet.com>
2019-05-09 18:22:46 +0200

    Añadido ejemplo PetStore

--- inc/functions.php
+++ inc/functions.php
@@ -8,7 +8,7 @@
 			$html_menu .= '<li class="nav-item dropdown">';
 
 		    $html_menu .= '<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		          Bancos
+		          ' . $key . '
 		        </a>';
 
 		    $html_menu .= '<div class="dropdown-menu" aria-labelledby="navbarDropdown">';
--- inc/settings.php
+++ inc/settings.php
@@ -1,6 +1,6 @@
 <?php
 
-	define('BASE_URL', 'https://respirainternet.com/redoc/' );
+	define('BASE_URL', 'https://respirainternet.com/redoc/' ); // the url where you uploaded the files
 
 	define('LOGO_URL', 'https://redoc.ly/wp-content/uploads/2018/05/redocly_medium_white.png');
 
